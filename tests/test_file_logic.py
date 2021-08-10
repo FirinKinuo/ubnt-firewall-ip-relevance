5cae2aa1a24123a25aca160c3eee60e2fe6db734
FirinKinuo <firinkinuo@protonmail.ch>
2021-08-10 12:12:26 +0300

    refactor tests

--- tests/test_file_logic.py
+++ tests/test_file_logic.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from pathlib import Path
 from app.file import *
 
@@ -7,11 +8,16 @@ from app.file import *
 def create_test_file():
     test_file = Path("temp_test.txt")
     with open(test_file, 'w') as host_file:
-        host_file.write("google.com\nsberbank.ru\nvk.com")
+        host_file.write("sberbank.ru\nvk.com")
 
-    return test_file
+    yield test_file
+
+    os.remove(test_file)
 
 
 def test_search_new_host_from_file(host_file):
     host_file = HostFile(host_file_path=host_file)
-    assert host_file.find_new_host() == ['sberbank.ru', 'vk.com']
+
+    new_hosts = host_file.find_new_host()
+
+    assert new_hosts == ['sberbank.ru', 'vk.com']
--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -2,6 +2,8 @@ from app.lookup import *
 
 
 def test_find_id_by_hostname():
-    assert find_all_ip_hostname("sberbank.ru") == ['194.54.14.168']
-    assert find_all_ip_hostname('google.com') == ['142.251.1.139', '142.251.1.101', '142.251.1.113', '142.251.1.138',
-                                                  '142.251.1.102', '142.251.1.100']
+    host_payload = "localhost"
+
+    ip_host = find_all_ip_hostname(hostname=host_payload)
+
+    assert ip_host == ['127.0.0.1']
