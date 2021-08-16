8f1dddec17c4ed635e02c488f278d9fd5f36e283
FirinKinuo <deals@fkinuo.ru>
2021-08-16 15:38:14 +0300

    add search for removal of IP from the file with the list of hosts

--- app/file/file.py
+++ app/file/file.py
@@ -1,9 +1,10 @@
-from app.database import Database
-from os import environ
+from app.setting import DATABASE
+import re
 
 
 class HostFile:
     """Класс для работы с файлом списка хостов"""
+
     def __init__(self, host_file_path: str):
         self.host_file_path = host_file_path
 
@@ -14,13 +15,25 @@ class HostFile:
             list: Список добавленных хостов
         """
 
-        db = Database(database=environ.get("SQLITE_PATH"))
-        hostname_db = db.get_host_list()
+        hostname_db = DATABASE.get_host_list()
 
         with open(self.host_file_path, 'r') as host_file:
             # Читаем список хостов из файла и разделяем их по символу переноса строки
             read_hosts = host_file.read().split("\n")
             # Отделяем хосты, которые занесены в БД
-            return [host for host in read_hosts if host not in hostname_db]
+            return [re.sub(r'(\s+)', '', host) for host in read_hosts if
+                    host not in [host.hostname for host in hostname_db] and re.match(r".+\..+", host) is not None]
 
+    def find_deleted_host(self) -> list:
+        """
+        Поиск хостов, что были удалены из файла
+        Returns:
+            list: Список хостов, которые были удалены
+        """
+        hostname_db = DATABASE.get_host_list()
 
+        with open(self.host_file_path, 'r') as host_file:
+            # Читаем список хостов из файла и разделяем их по символу переноса строки
+            read_hosts = host_file.read().split("\n")
+            # Ищем разницу между тем, что в файле и в базе данных
+            return [host for host in hostname_db if host.hostname not in read_hosts]
--- app/file/file_watchdog.py
+++ app/file/file_watchdog.py
@@ -3,19 +3,26 @@ from watchdog.events import PatternMatchingEventHandler
 from pathlib import Path
 from app.file import HostFile
 from app.lookup import find_all_ip_hostname
-from app.utils import add_new_ip_all_service
+from app.logger import get_logger
+from app.utils import add_new_ip_all_service, delete_host_from_all_service
+
+
+log = get_logger(__name__)
 
 
 class HostFileWatchdog:
     """Класс для работы со слежением за файлом с хостами"""
-
     def __init__(self, file_path: [Path, str]):
         self.file_path = Path(file_path)
         self.observer = Observer()
 
         # Если файла с хостами не существует - создать его
         if not self.file_path.exists():
-            self.file_path.touch()
+            log.warning(f"По адресу {self.file_path} не обнаружен файл, будет создан новый")
+            try:
+                self.file_path.touch()
+            except OSError as err:
+                log.error(f"Ошибка при создании файла: {err}")
 
         hostname_handler = self.HostFileHandler(
             patterns=[self.file_path.parts[-1]],
@@ -26,7 +33,6 @@ class HostFileWatchdog:
 
     class HostFileHandler(PatternMatchingEventHandler):
         """Класс слежения за изменениями в файле"""
-
         def on_modified(self, event):
             """Обрабатывает ивенты при изменениях в файле"""
             self.update_ip_table(event.src_path)
@@ -44,13 +50,16 @@ class HostFileWatchdog:
             host_file = HostFile(host_file_path=file_path)
             new_hosts = host_file.find_new_host()
             if new_hosts:
+                log.info(f"Обнаружено добавление хостов: {', '.join(new_hosts)}")
                 for host in new_hosts:
-                    # Проверка на пустые строки и символы переноса
-                    if host in ('', ' ', '\n', '\t'):
-                        continue
-
                     ip_list = find_all_ip_hostname(hostname=host)
                     add_new_ip_all_service(hostname=host, ip_address_list=ip_list)
+
+            deleted_hosts = host_file.find_deleted_host()
+            if deleted_hosts:
+                log.info(f"Обнаружено удаление хостов: {', '.join([host.hostname for host in deleted_hosts])}")
+                for host in deleted_hosts:
+                    delete_host_from_all_service(hostname=host)
 
     async def start(self) -> None:
         """
@@ -58,5 +67,5 @@ class HostFileWatchdog:
         Returns:
             None:
         """
-
         self.observer.start()
+        log.info(f"Запущено слежение за файлом {self.file_path}")
